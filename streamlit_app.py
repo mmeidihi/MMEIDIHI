f39580bea0b40a8be6c7ae65d5b8e19828d4b092
mmeidihi <146219809+mmeidihi@users.noreply.github.com>
2024-06-28 04:13:33 +0000

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,23 @@
-import streamlit as st
+import streamlit as st 
+import pandas as pd 
+#import requests
+#from st_aggrid import AgGrid
 
-def main() :
-    st.header('Halaman Streamlit Muh. Meidi H.I.')
-    st.subheader('This is SubHeader')
-    st.markdown('# Rendering Markdown ')
-    st.write('Some Phytagorean Equation : ')
-    st.latex('c^2 = a^2+b^2')
+house = pd.read_csv('house_clean.csv')
 
-if __name__== '__main__':
-    main()
+def main() : 
+  st.write('Minimal Example')
+
+if _name_ == '_main_' : 
+  main()
+
+def main() : 
+  st.header('Halaman Streamlit Alif')
+  st.subheader('This is SubHeader')
+  st.markdown('# Rendering Markdown ')
+  st.write('Some Phytagorean Equation : ')
+  st.latex('c^2 = a^2+b^2')
+
+  st.dataframe(house)
+if _name_ == '_main_' : 
+  main()
