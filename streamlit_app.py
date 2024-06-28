8e547dad13765cdff37a9edccd9df4f97098dec8
mmeidihi <146219809+mmeidihi@users.noreply.github.com>
2024-06-28 06:10:09 +0000

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 import pandas as pd 
-#import requests
-#from st_aggrid import AgGrid
+import requests
+from st_aggrid import AgGrid
 
 house = pd.read_csv('house_clean.csv')
 
@@ -13,5 +13,13 @@ def main() :
   st.latex('c^2 = a^2+b^2')
 
   st.dataframe(house)
+
+  st.write('Metrics')
+  st.metric(label="Temperature", value="70 °F", delta="-1.2 °F")
+
+  st.write('Menampilkan Dataframe dengan St AgGrid')
+  AgGrid(house)
+  st.table([x for x in range(1,5)])
+
 if __name__ == '__main__' : 
   main()
