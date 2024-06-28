361c2cd86bf08f594b34824171e1b57c7e137fe1
mmeidihi <146219809+mmeidihi@users.noreply.github.com>
2024-06-28 06:15:42 +0000

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ def main() :
   st.dataframe(house)
 
   st.write('Metrics')
-  st.metric(label="Temperature", value="70 °F", delta="-1.2 °F")
+  st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
 
   st.write('Menampilkan Dataframe dengan St AgGrid')
   AgGrid(house)
