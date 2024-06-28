50ee44dc9546d5cf1056faff07381f990fb7a08f
mmeidihi <146219809+mmeidihi@users.noreply.github.com>
2024-06-28 03:10:16 +0000

    main

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
 def main() :
-    st.write('Minimal Example')
     st.header('This is Header')
+    st.subheader('This is SubHeader')
+    st.markdown('# Rendering Markdown ')
+    st.write('Some Pjytagorean Equation : ')
+    st.latex('c^2 = a^2+b^2')
 
-if _name_== '_main_':
+if __name__== '__main__':
     main()
