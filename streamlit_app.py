9e64ac94cbbcb054f307dfe08df655f0b50d0396
mmeidihi <146219809+mmeidihi@users.noreply.github.com>
2024-06-28 06:52:45 +0000

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ def main() :
   st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
 
   st.write('Menampilkan Dataframe dengan St AgGrid')
-  AgGrid(house)
+  #AgGrid(house)
   st.table([x for x in range(1,5)])
 
   click_me_btn = st.button('Click Me')
@@ -37,6 +37,28 @@ def main() :
   #Input (Typing)
   num_input = st.number_input('Input Berapapun')
   st.write('Kuadrat dari {} adalah {}'.format(num_input,num_input**2))
+
+  ##### Menampilkan menu sidebar 
+  sidebar_checkbox = st.sidebar.checkbox('Checkbox di Sidebar')
+  sidebar_radio_button = st.sidebar.radio('Pilih Menu',options=['A','B','C'])
+
+  #columns :
+  col1, col2, col3 = st.columns(3)
+
+  with col1:
+      st.header("A cat")
+      st.image("https://static.streamlit.io/examples/cat.jpg")
+  #atau dengan assignment 
+  image_col1 = col1.image("https://static.streamlit.io/examples/cat.jpg")
+
+  with col2:
+      st.header("A dog")
+      st.image("https://static.streamlit.io/examples/dog.jpg")
+
+  with col3:
+      st.header("An owl")
+      st.image("https://static.streamlit.io/examples/owl.jpg")
       
+
 if __name__ == '__main__' : 
   main()
